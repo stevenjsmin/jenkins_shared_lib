3714363c322c4ad77ada081e77a108c89d823a35
Steven Min <steven.jsmin@gmail.com>
2020-08-19 00:53:13 +1000

    Create for Test
    For ping pong

---
+++ vars/pong.groovy
@@ -0,0 +1,22 @@
+def call(Map config) {
+
+    echo "-------------------------------------------------------------------------------------------------- pong.groovy"
+
+    // def ARTIFACT_SUB_DIR   = config.ARTIFACT_SUB_DIR
+    // def FILE_NAME          = config.FILE_NAME
+    
+    
+    ansiColor('gnome-terminal') {
+          timestamps {
+
+              sh """
+                  export ANSIBLE_FORCE_COLOR=true
+                  export PYTHONUNBUFFERED=1
+                  echo "Hi from shell command~~~"
+              """
+              
+          }
+    }
+    
+}        
+        
